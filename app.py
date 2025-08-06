beeb21d1bd534cd3aabdfcf16b6c593e339ad3e7
vmlcode <67714035+vmlcode@users.noreply.github.com>
2025-08-06 15:26:07 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import pandasai as pai
+from dotenv import load_dotenv
+from pandasai_openai import AzureOpenAI
+import os
+
+load_dotenv()
+
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+AZURE_OPENAI_API_TOKEN = os.getenv("AZURE_OPENAI_API_TOKEN")
+AZURE_OPENAI_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
+
+llm = AzureOpenAI(
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    api_token=AZURE_OPENAI_API_TOKEN,
+    deployment_name=AZURE_OPENAI_DEPLOYMENT_NAME,
+    api_version=AZURE_OPENAI_API_VERSION
+)
+
+pai.config.set({
+    "llm": llm,
+    "save_logs": False,
+    "save_charts": False
+})
+
+df = pai.read_excel("./example_files/Electric_Vehicle_Population_Data copy.xlsx")
+print(df.head)
+response = df.chat("ALWAYS CREATE JUST TEXT NOT CHARTS OR CODE: Please create a brief descriptive summary of this excel file")
+print(f"llm response: {response}")
+
